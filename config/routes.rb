c128493e8f4cac85702e1bb9212ae49155784c4e
moinali5599 <moinali5599@gmail.com>
2020-03-20 18:29:21 +0530

    Added route to user

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   get "todos", to: "todos#index"
   get "todos/create", to: "todos#create"
   get "todos/:id", to: "todos#show"
-
+  resources :user
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
