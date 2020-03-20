03e5a5d04ac20e6a09ba3c1d1a882e0ec64a3f97
moinali5599 <moinali5599@gmail.com>
2020-03-20 18:39:35 +0530

    Added login feature

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -14,4 +14,8 @@ class UsersController < ApplicationController
     new_user = User.create!(name: params[:name], email: email, passowrd: password)
     render plain: new_user.to_s
   end
+
+  def login
+    render plain: User.where("email = ? and password = ?", params[:email], params[:password].exits?)
+  end
 end
