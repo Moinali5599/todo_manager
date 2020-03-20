54df463d9afd979cfa98bfef5a198e508054b225
moinali5599 <moinali5599@gmail.com>
2020-03-20 18:27:36 +0530

    Created users controller

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,21 @@
+class UsersController < ApplicationController
+  skip_before_action :verify_authenticity_token
+
+  def index
+    render plain: User.all.map { |user| user.to_s }.join("\n")
+  end
+
+  def show
+    user = User.find(params[:id])
+    render plain: user.to_s
+  end
+
+  def create
+    new_user = User.create!(name: params[:name], email: email, passowrd: password)
+    render plain: new_user.to_s
+  end
+
+  def login
+    render plain: User.where("email = ? and password = ?", params[:email], params[:password].exits?)
+  end
+end
